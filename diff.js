c0e194b6086a9fddb900dda5d97135944d53c7a3
shivram yadav <shivramyadav52@gmail.com>
2022-07-09 14:34:00 +0530

    multiply file added

---
+++ diff.js
@@ -0,0 +1,4 @@
+const diff = (a, b) => {
+  return a - b;
+};
+console.log("sum of two numbers", diff(1, 2));
---
+++ multiply.js
@@ -0,0 +1,4 @@
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log("sum of two numbers", multiply(1, 2));
